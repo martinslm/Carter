6304b8f006eb79a7334c199faf22e0ecfb056a27
Larissa Martins Mauricio <larissa.jucre@gmail.com>
2019-04-23 22:00:56 -0300

    Referências Material Design
    Referências Material Design

---
+++ Carter/Telas/Login.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Carter
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
